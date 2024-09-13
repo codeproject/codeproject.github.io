7cb612414c09fc3708593d95f8dcbea2ba5e5d7f
Chris Maunder <chris.maunder@codeproject.com>
2024-09-13 10:21:24 -0400

    Initial github.io pages

--- mkdocs/build_site.bat
+++ mkdocs/build_site.bat
@@ -4,5 +4,6 @@ REM Assuming we're in the /mkdocs directory
 call .\bin\windows\python39\venv\Scripts\activate.bat
 pushd CodeProject.AI
 python -m mkdocs build
+robocopy /e .\site ..\..\codeproject.ai
 popd
 call .\bin\windows\python39\venv\Scripts\deactivate.bat
