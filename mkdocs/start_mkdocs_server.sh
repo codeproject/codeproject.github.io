9b37919e43997a75c9d796d6e2fdde54032fa819
Chris Maunder <chris.maunder@hotmail.comdotnet>
2024-11-20 22:04:43 -0500

    Corrected linux setup

--- mkdocs/start_mkdocs_server.sh
+++ mkdocs/start_mkdocs_server.sh
@@ -1,19 +1,19 @@
 # Assuming we're in the /docs/mkdocs directory
 
 if [[ $OSTYPE == 'darwin'* ]]; then
-    bash ../bin/macos/python39/venv/bin/activate
+    bash ./bin/macos/python39/venv/bin/activate
 else
-    bash ../bin/linux/python39/venv/bin/activate
+    bash ./bin/linux/python39/venv/bin/activate
 fi
 
 pushd CodeProject.AI
 
 if [[ $OSTYPE == 'darwin'* ]]; then
     open http://127.0.0.1:8000/ &
-    ../../bin/macos/python39/venv/bin/python3 -m mkdocs serve
+    ../bin/macos/python39/venv/bin/python3 -m mkdocs serve
 else
     xdg-open http://127.0.0.1:8000/ &
-    ../../bin/linux/python39/venv/bin/python3 -m mkdocs serve
+    ../bin/linux/python39/venv/bin/python3 -m mkdocs serve
 fi
 
 popd
